57e5f88fc1f001375fa171c9dc098efb2bca06ad
htyagi5 <harshit2310079@akgec.ac.in>
2026-02-03 10:28:38 +0530

    fixied the cors issue

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const httpserver=createServer(app)
 
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173",
+        "https://multiplayer-brush-sync.vercel.app",
+        "https://multiplayer-brush-sync-git-main-harshiis-projects.vercel.app",
+        "https://multiplayer-brush-sync-5a2cksu4c-harshiis-projects.vercel.app"
+    ],
 }))
 
 const peerServer=new PeerServer({
