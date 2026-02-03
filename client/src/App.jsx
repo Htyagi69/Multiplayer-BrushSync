a00d1cad0bc22f4e8358d7a6f8b294734e304e48
Htyagi69 <contacttyagiharshit@gmail.com>
2026-02-03 21:05:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import { io } from 'socket.io-client';
 function MainLayout(){
    const {roomid}=useParams()
       const [cursors, setCursors] = useState({}); // { userId: {x, y} }
-        const socket=useMemo(()=>io('https://multiplayer-brush-sync.vercel.app'),[]);
+        const socket=useMemo(()=>io('https://multiplayer-brushsync.onrender.com'),[]);
 
         // --- DRAWING SYNC LOGIC ---
     const handleMount = (editor) => {
