b5814d40d6213f5d83b548af784d1ac9477f0706
Htyagi69 <contacttyagiharshit@gmail.com>
2026-04-11 21:13:14 +0530

    Update auth.js

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -7,6 +7,8 @@ export const auth = betterAuth({
    database: new Pool({
         connectionString:process.env.DATABASE_URL
      }),
+     secret: process.env.BETTER_AUTH_SECRET,
+     baseURL: process.env.BETTER_AUTH_URL,
        emailAndPassword: { 
        enabled: true, 
        autoSignIn:true,
