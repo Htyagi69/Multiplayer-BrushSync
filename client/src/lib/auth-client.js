fca4bb06419d2fe3d4ee55a3bdfafa73901a02f6
Htyagi69 <contacttyagiharshit@gmail.com>
2026-04-11 23:59:48 +0530

    Update auth-client.js

--- client/src/lib/auth-client.js
+++ client/src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.PROD 
-    ? "/api/auth"                        // production - Vercel proxy
+    ? "https://multiplayer-brush-sync.vercel.app/api/auth"                       // production - Vercel proxy
     : "http://localhost:3000/api/auth",  // local dev
   fetchOptions: {
     credentials: 'include',
