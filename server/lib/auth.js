90138f0b5066893c67551169f9dd6c01a7290636
Htyagi69 <contacttyagiharshit@gmail.com>
2026-04-12 14:12:20 +0530

    Update auth.js

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -19,7 +19,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
     }, 
   },
-    baseURL: "https://multiplayer-brush-sync.vercel.app",
   trustedOrigins:["https://multiplayer-brush-sync.vercel.app"],
      advanced: {
     defaultCookieAttributes: {
