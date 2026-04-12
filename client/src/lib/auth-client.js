880c3ae2b8c4afd609941954c52d2ffd7444a77f
Htyagi69 <contacttyagiharshit@gmail.com>
2026-04-12 14:10:04 +0530

    Update auth-client.js

--- client/src/lib/auth-client.js
+++ client/src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     // baseURL: "http://localhost:3000",
-    baseURL: "https://multiplayer-brushsync-1.onrender.com/api/auth",
+    baseURL: "https://multiplayer-brush-sync.vercel.app",
     fetchOptions:{
         credentials:'include'
     }
