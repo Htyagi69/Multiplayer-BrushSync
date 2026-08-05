8112d72925d6fcae4e06d2c07b5cd0ea85fa12f8
Htyagi69 <contacttyagiharshit@gmail.com>
2026-08-05 09:48:04 +0530

    solves the url issue between production and local development

--- client/src/lib/auth-client.js
+++ client/src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     // baseURL: "http://localhost:3000",
-    baseURL: "https://multiplayer-brush-sync.vercel.app",
+    baseURL: import.meta.env.VITE_SERVER_URL,
     fetchOptions:{
         credentials:'include'
     }
