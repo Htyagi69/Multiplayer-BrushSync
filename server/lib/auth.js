5b94e3eb82d1b4477c1ff972c5912d62859e8f61
Htyagi69 <contacttyagiharshit@gmail.com>
2026-08-05 19:46:45 +0530

    dynamic url using env

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -19,8 +19,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
     }, 
   },
-  trustedOrigins:[process.env.CLIENT_URL],
-  // trustedOrigins:["https://multiplayer-brush-sync.vercel.app"],
+  trustedOrigins:[process.env.VITE_CLIENT_URL],
      advanced: {
     defaultCookieAttributes: {
       sameSite: "none",
