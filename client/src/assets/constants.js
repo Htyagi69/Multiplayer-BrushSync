cf85a7c03ea9b62aaadad26bc0fe75b48a9cc637
Htyagi69 <contacttyagiharshit@gmail.com>
2026-08-07 20:44:30 +0530

    added a static images as an options to change background

---
+++ client/src/assets/constants.js
@@ -0,0 +1,12 @@
+export const  backGroundImages=[
+   {1:"wolfy.jpg"},
+   {2:"3Dbackground.jpg"},
+   {3:"cartoon.jpg"},
+   {4:"cartoonfamily.jpg"},
+   {5:"droplets.jpg"},
+   {6:"galaxy.jpg"},
+   {7:"ladies.jpg"},
+   {8:"hotairballon.jpg"},
+   {9:"moon.jpg"},
+   {10:"spacebunch.jpg"},
+]
